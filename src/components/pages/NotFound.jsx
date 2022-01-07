610fe850f309c372bf7472629d1169c763132913
Handika Harianto Ew Jong <handikaharianto01@gmail.com>
2022-01-07 10:54:56 +0700

    Add content inside  404 page

--- src/components/pages/NotFound.jsx
+++ src/components/pages/NotFound.jsx
@@ -1,8 +1,18 @@
+import { FaHome } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
 function NotFound() {
   return (
-    <div>
-      <h1>not found page</h1>
-    </div>
+    <section className='not-found'>
+      <div className='text-center'>
+        <h1 className='text-8xl font-bold'>Oops!</h1>
+        <p className='text-5xl my-7'>404 - Page Not Found!</p>
+        <Link to='/' className='btn btn-primary '>
+          <FaHome className='mr-1' />
+          BACK TO HOME
+        </Link>
+      </div>
+    </section>
   )
 }
 
