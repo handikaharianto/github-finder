700413b225e22fdbf3317322d7768eb9735c27eb
Handika Harianto Ew Jong <handikaharianto01@gmail.com>
2022-01-07 10:09:12 +0700

    Add routes for each page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import NotFound from './components/pages/NotFound'
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar title='GitHub Finder' />
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar title='GitHub Finder' />
+        <main className='container px-3 pb-12'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </BrowserRouter>
   )
 }
